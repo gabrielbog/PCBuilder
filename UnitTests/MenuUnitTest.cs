1dc5ae6e71c20d716e64d0a358297489864d1559
gabrielbog <bogoslovgabriel@gmail.com>
2024-04-07 11:05:36 +0300

    Added unit tests

---
+++ UnitTests/MenuUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using PCBuilder.Classes.Factories;
+using PCBuilder.Classes.Menu;
+using PCBuilder.Classes.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class MenuUnitTest
+    {
+        [Fact]
+        public void GetInstance_Menu_ReturnsSameInstance()
+        {
+            //Arrange
+            var expectedInstance = Menu.GetInstance();
+
+            //Act
+            var instance = Menu.GetInstance();
+
+            //Assert
+            Assert.Equal(expectedInstance, instance);
+        }
+    }
+}
